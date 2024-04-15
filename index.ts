063f4129511e3ff4700e835de422da97f34a9d86
Nat Valentine <sodiumvii@gmail.com>
2024-04-15 18:23:54 -0300

    add more tests

--- index.ts
+++ index.ts
@@ -8,7 +8,6 @@ export function reverse(str:String): String {
     return reversed;
 }
 
-
 // Reverse a number
 export function reverseInt(num:number): number {
     let reversed = num.toString().split('').reverse().join('');
@@ -16,12 +15,10 @@ export function reverseInt(num:number): number {
     return parseInt(reversed) * Math.sign(num);
 }
 
-
 // Palindrome
 export function isPalindrome(str:String): boolean {
     return str === reverse(str);
 }
-
 
 // Most common char
 export function mostCommonChar(str:String): String {
@@ -42,11 +39,8 @@ export function mostCommonChar(str:String): String {
     return maxChar;
 }
 
-
 // Most repeated word
-// maxRepeatedWord("hola caracola hola cara hola cola") => {'hola': 3}
-// maxRepeatedWord("hola hola caracola cola cola cara hola cola") => [{'hola': 3}, {'cola', 3}]
-function maxRepeatedWord(str) {
+export function maxRepeatedWord(str) {
     const wordMap = {}
     const words = str.split(" ");
 
@@ -71,13 +65,8 @@ function maxRepeatedWord(str) {
     return maxWords;
 }
 
-//console.log(maxRepeatedWord("hola hola caracola cola cola cara hola cola"));
-
-
-
 // Array chunks
-// chunk([1, 2, 3, 4, 5], 2) => [[1, 2], [3, 4], [5]]
-function chunk(array, size) {
+export function chunk(array, size) {
     const result = [];
     let index = 0;
     while (index < array.length) {
@@ -88,12 +77,8 @@ function chunk(array, size) {
     return result;
 }
 
-// console.log(chunk([1, 2, 3, 4, 5], 2));
-
-
 // Capitalize a string
-// capitalize("hello, world") => "Hello, World"
-function capitalize(str) {
+export function capitalize(str) {
     const words: Array<string> = str.split(' ');
     const result = [];
     for (let word of words) {
@@ -103,13 +88,8 @@ function capitalize(str) {
     // alt: return words.map(word => word[0].toUpperCase() + word.slice(1).join(' '););
 }
 
-// console.log(capitalize("hello, world"));
-
-
-
 // Anagram
-// isAnagram("hello world", "world hello") => true
-function isAnagram(str1:string, str2:string) {
+export function isAnagram(str1:string, str2:string) {
     const charMap1 = charMap(str1);
     const charMap2 = charMap(str2);
    
@@ -131,13 +111,8 @@ function charMap(str: string) {
     return charMap;
 }
 
-// console.log(isAnagram("hello world", "world hello"));
-
-
-
 // Count vowels
-// countVowels("Hello World") => 3
-function countVowels(str) {
+export function countVowels(str) {
     const vowels = ['a', 'e', 'i', 'o', 'u'];
     let count = 0;
     
@@ -155,13 +130,9 @@ function countVowels(str) {
      */
 }
 
-// console.log(countVowels("Hello World"));
-
-
-
 // Fizz Buzz
 // log numbers from 1 to n. on multiples of 3 print fizz, on multiples of 5 print buzz.
-function fizzbuzz(n) {
+export function fizzbuzz(n) {
     for (let i = 1; i < n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("fizzbuzz");
@@ -177,10 +148,8 @@ function fizzbuzz(n) {
 
 // fizzbuzz(20);
 
-
-
 // Steps string pattern
-function steps(n) {
+export function steps(n) {
     for (let row = 1; row <= n; row++) {
         let line = '';
         for (let col = 1; col <= n; col++) {
@@ -196,10 +165,8 @@ function steps(n) {
 
 // steps(4);
 
-
-
 // Pyramid string pattern
-function pyramid(n) {
+export function pyramid(n) {
     const mid = Math.floor((2 * n - 1) / 2);
     for (let row = 0; row < n; row++) {
         let line = '';
@@ -216,11 +183,9 @@ function pyramid(n) {
 
 // pyramid(5);
 
-
-
 // Spiral matrix
 // a function that accepts an integer n and returns a nxn spiral matrix.
-function spiralMatrix(n) {
+export function spiralMatrix(n) {
     const result = [];
     let counter = 1;
     let startRow = 0;
@@ -263,12 +228,10 @@ function spiralMatrix(n) {
 
 // console.log(spiralMatrix(4));
 
-
-
 // Fibonacci
 // first n numbers of fibonacci sequence
 // fibonacci(9) => 0 1 1 2 3 5 8 13 21
-function fibonacci(n) {
+export function fibonacci(n) {
     const array = [];
     for (let i = 0; i < n; i++) {
         if (i === 0) {
@@ -278,14 +241,12 @@ function fibonacci(n) {
         } else { array.push(array[i-1] + array[i-2]); }
     }
     return array;
+
+    /** alt:
+      Array.from({ length: n }).reduce(
+       (prevValue: Array<number>, currentValue: number, i: number) => 
+           prevValue.concat(i > 1 ? prevValue[i - 1] + prevValue[i - 2] : i),
+       []
+      );
+     */
 }
-
-const altFibonacci = (n) =>
-  Array.from({ length: n }).reduce(
-    (prevValue: Array<number>, currentValue: number, i: number) => 
-        prevValue.concat(i > 1 ? prevValue[i - 1] + prevValue[i - 2] : i),
-    []
-  );
-
-//console.log(fibonacci(9));
-//console.log(altFibonacci(9));
--- tests.test.ts
+++ tests.test.ts
@@ -1,4 +1,4 @@
-import { isPalindrome, mostCommonChar, reverse, reverseInt } from ".";
+import { capitalize, chunk, countVowels, isAnagram, isPalindrome, maxRepeatedWord, mostCommonChar, reverse, reverseInt } from ".";
 
 describe('Reverse a string', () => {
     test('hello world reversed is dlrow olleh', () => {
@@ -32,3 +32,37 @@ describe('Most common char', () => {
         expect(mostCommonChar('aaaaabbbcccc')).toBe('a');
     })
 });
+
+describe('Most repeated word', () => {
+    test('in hola caracola hola cara hola cola, hola appears 3 times', () => {
+        expect(maxRepeatedWord('hola caracola hola cara hola cola')).toEqual([{'hola': 3}]);
+    }),
+    test('in hola hola caracola cola cola cara hola cola, hola and cola appear each 3 times', () => {
+        expect(maxRepeatedWord('hola hola caracola cola cola cara hola cola')).toEqual([{'hola': 3}, {'cola': 3}]);
+    })
+});
+
+describe('Array chunks', () => {
+    test('given an array and a chunk size, it is separated into chunks', () => {
+        expect(chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]]);
+    })
+});
+
+describe('Capitalize a string', () => {
+    test('hello world capitalized is Hello World', () => {
+        expect(capitalize('hello world')).toBe('Hello World');
+    })
+});
+
+describe('Anagram', () => {
+    test('world hello is an anagram of hello world', () => {
+        expect(isAnagram('world hello', 'hello world')).toBe(true);
+    })
+});
+
+describe('Counting vowels', () => {
+    test('hello world has 3 vowels', () => {
+        expect(countVowels('hello world')).toBe(3);
+    })
+});
+
